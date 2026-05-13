132db6bba4dfc38ef7d40c8febed0efdddfea772
BramObae <103332582+BramObae@users.noreply.github.com>
2026-05-13 20:35:05 +0300

    Update Footer.tsx

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -1,6 +1,6 @@
 import { Instagram, Facebook, Twitter, Mail, MapPin, Phone } from "lucide-react";
 import { Link } from "react-router-dom";
-import logo from "@/assets/logo1.jpg";
+import logo1 from "@/assets/logo1.jpg";
 
 const SOCIALS = [
   { Icon: Instagram, href: "https://www.instagram.com/fcmhpacademyke", label: "Instagram" },
